3bce73329d4f6fc02dfdcb9daa0161733eba9e31
poojasavalikar <120164610+poojasavalikar@users.noreply.github.com>
2022-12-22 12:40:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,6 @@ import streamlit
 streamlit.title('This is my parents new healthy Diner')
 
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Sandwich')
-streamlit.text('Garlic Bread')
-streamlit.text('Burger')
-
-
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')

a801e5712ed63efd2be711626b4fbaadab300e6a
poojasavalikar <120164610+poojasavalikar@users.noreply.github.com>
2022-12-22 12:39:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,10 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Sandwich')
 streamlit.text('Garlic Bread')
 streamlit.text('Burger')
+
+
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')

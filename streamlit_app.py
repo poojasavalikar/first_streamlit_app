a9e038b28aaacdccc666a9b5b01ff3fc7b24bf9e
poojasavalikar <120164610+poojasavalikar@users.noreply.github.com>
2023-03-07 11:42:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@ import streamlit
 
 streamlit.title('This is my parents new healthy Diner')
 
+streamlit.header('Breakfast favourites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 hard boiled free range egg')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')

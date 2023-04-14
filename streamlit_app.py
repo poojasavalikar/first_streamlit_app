5041fade061e58f870b81c4422e30b5d8b078d66
poojasavalikar <120164610+poojasavalikar@users.noreply.github.com>
2023-04-14 20:09:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ def get_fruityvice_data(this_fruit_choice):
     return (fruityvice_normalized) 
 
 streamlit.header ('Fruityvice Fruit Advice!')
-def get_fruit_load)list():
+def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * from fruit_load_list")
     return my_cur.fetchall()
